f4dbb623684e3a1d686ce393d41c0bfc086d6fec
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-11-28 22:50:53 +0100

    Reformat to pass flake8

--- tests/unit/test_parser.py
+++ tests/unit/test_parser.py
@@ -6,7 +6,11 @@ from src_python import parser as m_parser
 
 class TestLineParser(unittest.TestCase):
     def setUp(self):
-        line = '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-" "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
+        line = (
+            '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-"'
+            ' "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+            ' (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
+        )
         self.parser = m_parser.LineParser(line)
 
     def test_remote_addr(self):
@@ -48,7 +52,10 @@ class TestLineParser(unittest.TestCase):
 
     def test_http_user_agent(self):
         self.assertEqual(
-            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+            (
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"
+                " (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+            ),
             self.parser.http_user_agent,
         )
 
