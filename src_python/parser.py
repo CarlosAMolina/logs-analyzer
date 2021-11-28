a48311affb1326186f54a224a042b0b1f991db9c
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-11-28 21:41:54 +0100

    Parse nginx log

--- src_python/parser.py
+++ src_python/parser.py
@@ -1,20 +1,67 @@
-import datetime
+import datetime as datetime
+import re
 from typing import List
 
 
 class Log:
-    def __init__(self, ip: str, datetime_: datetime.datetime):
-        self.ip = ip
-        self.datetime_ = datetime_
+    def __init__(self, remote_addr: str, time_local: datetime.datetime):
+        self.remote_addr = remote_addr
+        self.time_local = time_local
 
 
 class LineParser:
+    """
+    https://docs.nginx.com/nginx/admin-guide/monitoring/logging/
+    """
+
     def __init__(self, line: str):
-        self._line = line
+        regex = r'^(([0-9]{1,3}[\.]){3}[0-9]{1,3})\s-\s(.+)\s\[(.*)\]\s"(.*)"\s(\d{1,3})\s(\d+)\s"(.*)"\s"(.*)"'
+        self._match = re.search(regex, line)
 
     @property
-    def ip(self):
-        return "8.8.8.8"
+    def remote_addr(self) -> str:
+        return self._match.group(1)
+
+    @property
+    def remote_user(self) -> str:
+        return self._match.group(3)
+
+    @property
+    def time_local(self) -> datetime.datetime:
+        result = self._get_time_local_as_str()
+        return self._get_time_local_as_datetime(result)
+
+    def _get_time_local_as_str(self) -> str:
+        return self._match.group(4)
+
+    def _get_time_local_as_datetime(self, datetime_str: str) -> datetime.datetime:
+        """
+        https://www.programiz.com/python-programming/datetime/strptime
+        """
+        format = "%d/%b/%Y:%H:%M:%S %z"
+        return datetime.datetime.strptime(datetime_str, format)
+
+    @property
+    def request(self) -> str:
+        return self._match.group(5)
+
+    @property
+    def status(self) -> int:
+        result = self._match.group(6)
+        return int(result)
+
+    @property
+    def body_bytes_sent(self) -> int:
+        result = self._match.group(7)
+        return int(result)
+
+    @property
+    def http_referer(self) -> str:
+        return self._match.group(8)
+
+    @property
+    def http_user_agent(self) -> str:
+        return self._match.group(9)
 
 
 class FileParser:
@@ -28,9 +75,8 @@ class FileParser:
         with open(file, "r") as f:
             return [
                 Log(
-                    ip=self._parse_line(line).ip,
-                    datetime_=None,
+                    remote_addr=self._parse_line(line).remote_addr,
+                    time_local=None,
                 )
-                for line in f.readlines()[0]
+                for line in f.read().splitlines()
             ]
-
--- tests/unit/test_parser.py
+++ tests/unit/test_parser.py
@@ -1,22 +1,64 @@
+import datetime
 import unittest
 
 from src_python import parser as m_parser
 
+
 class TestLineParser(unittest.TestCase):
     def setUp(self):
-        self.parser = m_parser.LineParser(None)
+        line = '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-" "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
+        self.parser = m_parser.LineParser(line)
 
-    def test_get_ip(self):
-        self.assertEqual("8.8.8.8", self.parser.ip)
+    def test_remote_addr(self):
+        self.assertEqual("8.8.8.8", self.parser.remote_addr)
+
+    def test_remote_user(self):
+        self.assertEqual("abc", self.parser.remote_user)
+
+    def test_get_time_local_as_str(self):
+        self.assertEqual(
+            "28/Nov/2021:00:18:22 +0100", self.parser._get_time_local_as_str()
+        )
+
+    def test_time_local(self):
+        self.assertEqual(
+            datetime.datetime(
+                2021,
+                11,
+                28,
+                0,
+                18,
+                22,
+                tzinfo=datetime.timezone(datetime.timedelta(seconds=3600)),
+            ),
+            self.parser.time_local,
+        )
+
+    def test_request(self):
+        self.assertEqual("GET / HTTP/1.1", self.parser.request)
+
+    def test_status(self):
+        self.assertEqual(200, self.parser.status)
+
+    def test_body_bytes_sent(self):
+        self.assertEqual(77, self.parser.body_bytes_sent)
+
+    def test_http_referer(self):
+        self.assertEqual("-", self.parser.http_referer)
+
+    def test_http_user_agent(self):
+        self.assertEqual(
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+            self.parser.http_user_agent,
+        )
 
 
 class TestFileParser(unittest.TestCase):
-    def test_get_file_parsed(self):
+    def _test_get_file_parsed(self):
         parse_file = m_parser.FileParser()
         line_parsed = parse_file("tests/access.log")[0]
-        self.assertEqual("8.8.8.8", line_parsed.ip)
+        self.assertEqual("8.8.8.8", line_parsed.remote_addr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
