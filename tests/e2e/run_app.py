77af2b0d72900d0ee97693b99ba950d0d17ee89b
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-12-12 22:10:09 +0100

    Run the main app from src_python

--- tests/e2e/run_app.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-if __name__ == "__main__":
-    sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
-    from src_python import app
-
-    app.app.run(debug=True)
