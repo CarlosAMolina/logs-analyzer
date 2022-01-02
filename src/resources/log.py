0dae53b92246d42588bafb90182dd9405c2bc06f
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-01-03 00:01:48 +0100

    Partial commit: API add post method

--- src/resources/log.py
+++ src/resources/log.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
 
@@ -7,4 +8,10 @@ from ..logs import manager
 class LogListResource(Resource):
     def get(self):
         data = [log.data for log in manager.get_logs_from_file()]
+        return {"data": data}, HTTPStatus.OK
+
+    def post(self):
+        data = request.get_json()
+        file = data["file"]
+        data = [log.data for log in manager.get_logs_from_file(file)]
         return {"data": data}, HTTPStatus.OK
--- tests/unit/test_resources_log.py
+++ tests/unit/test_resources_log.py
@@ -1,18 +1,37 @@
 from http import HTTPStatus
 import unittest
 import mock
+import os
 
 from src.resources import log
 from tests import LOG_FILE
 
 
 class TestLogListResource(unittest.TestCase):
+    def setUp(self):
+        self.class_ = log.LogListResource()
+
     def test_get(self):
         with mock.patch("src.logs.manager.extractor.LOG_FILE", LOG_FILE):
-            class_ = log.LogListResource()
-            result = class_.get()
+            result = self.class_.get()
             self.assertEqual(2, len(result[0]["data"]))
             self.assertEqual(HTTPStatus.OK, result[1])
+
+    def test_post_with_file_that_exits(self):
+        class FakeRequest:
+            @staticmethod
+            def get_json():
+                return {
+                    "file": os.path.join(
+                        os.path.dirname(__file__), "../files/access.log"
+                    )
+                }
+
+        with mock.patch("src.logs.manager.extractor.LOG_FILE", LOG_FILE):
+            with mock.patch("src.resources.log.request", FakeRequest):
+                result = self.class_.post()
+                self.assertEqual(2, len(result[0]["data"]))
+                self.assertEqual(HTTPStatus.OK, result[1])
 
 
 if __name__ == "__main__":
