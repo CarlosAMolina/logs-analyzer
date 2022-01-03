1f01bf26e72fd1b5552777fc87073f39c2223de0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-01-03 01:24:24 +0100

    API post method test file does not exists

--- tests/unit/test_resources_log.py
+++ tests/unit/test_resources_log.py
@@ -33,6 +33,19 @@ class TestLogListResource(unittest.TestCase):
                 self.assertEqual(2, len(result[0]["data"]))
                 self.assertEqual(HTTPStatus.OK, result[1])
 
+    def test_post_with_file_that_does_not_exit(self):
+        class FakeRequest:
+            @staticmethod
+            def get_json():
+                return {"file": "/foo/bar"}
+
+        with self.assertRaises(FileNotFoundError) as cm:
+            with mock.patch("src.logs.manager.extractor.LOG_FILE", LOG_FILE):
+                with mock.patch("src.resources.log.request", FakeRequest):
+                    self.class_.post()
+        the_exception = cm.exception
+        self.assertIsInstance(the_exception, FileNotFoundError)
+
 
 if __name__ == "__main__":
     unittest.main()
