f563f0f17ed9d574d2d137fe6d25821986eaa039
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-01-19 20:15:36 +0100

    Front Angular add hide menu if not file analized

--- src/frontend-angular/src/app/app.component.ts
+++ src/frontend-angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { LogFileStorageService } from './log-file-storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Logs Analyzer';
+
+  constructor(public logFileStorageService: LogFileStorageService) { }
+
+  ngOnInit(): void {
+  }
 }
